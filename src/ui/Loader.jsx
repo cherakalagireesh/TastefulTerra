1085b135a4f44f6eb2440f808ac5ca4088190059
cherakalagireesh <135722119+cherakalagireesh@users.noreply.github.com>
2024-04-24 05:40:33 +0530

    Add changes

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div
-      class="self-center items-center inline-block h-8 w-8  md:h-9 md:w-9 lg:h-10 lg:w-10  animate-spin rounded-full border-4 md:border-[5px] border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] border-blue-700"
+      class="self-center items-center inline-block h-10 w-10  md:h-9 md:w-9 lg:h-10 lg:w-10  animate-spin rounded-full border-4 md:border-[5px] border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] border-blue-700"
       role="status"
     ></div>
   );
